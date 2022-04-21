54e3d2db641979cb027b9135422389d32fd11f3d
katrimari <katri.lepola@edu.keuda.fi>
2022-04-21 10:11:00 +0300

    Study Point eka versio

---
+++ StudyPoint_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Studypoint
+{
+    public partial class EtusivuFM : Form
+    {
+        public EtusivuFM()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInBT_Click(object sender, EventArgs e)
+        {
+            LogInPL.Visible = true;
+            EtusivuPL.Visible = false;
+        }
+
+        private void EtusivuBT_Click(object sender, EventArgs e)
+        {
+            LogInPL.Visible=false;
+            EtusivuPL.Visible=true;
+        }
+
+        private void RegistrationBT_Click(object sender, EventArgs e)
+        {
+            RekisteroidyPL.Visible = true;
+            EtusivuPL.Visible = false;
+        }
+
+        private void Etusivu02BT_Click(object sender, EventArgs e)
+        {
+            RekisteroidyPL.Visible=false;
+            EtusivuPL.Visible=true;
+        }
+
+        private void Registration02BT_Click(object sender, EventArgs e)
+        {
+            RekisteroidyPL.Visible = true;
+            LogInPL.Visible = false;
+        }
+    }
+}
