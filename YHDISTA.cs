d3d436984ae145c1c93329acc86d4cfaac4b9bfd
JyriTurkkila <90317750+JyriTurkkila@users.noreply.github.com>
2022-04-26 12:06:13 +0300

    Add files via upload

---
+++ YHDISTA.cs
@@ -0,0 +1,46 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studypoint
+{
+    /*
+     * Tämän luokan avulla muodostetaan yhteys MySql-tietokantaan.
+     * Mikäli et ole ladannut MySql Connectoria ja Xamppia, tee se nyt.
+     * Muista myös lisätä MySql.Data.MySqlClient -kirjasto alkuun
+     */
+    class YHDISTA
+    {
+        /*public string yhteyslause()
+        {
+            return "datasource=localhost; port=3306;username=root;password=;database=opiskelijat";
+        }*/
+        private MySqlConnection yhteys = new MySqlConnection("datasource=localhost; port=3306;username=root;password=;database=Studypoint;SSL Mode = None");
+        // Luodaan funktio yhteyttä varten
+        public MySqlConnection otaYhteys()
+        {
+            return yhteys;
+        }
+        // Luodaan funktio yhteyden avaamista varten - HUOM! System.Data -kirjasto
+        public void avaaYhteys()
+        {
+            if (yhteys.State == ConnectionState.Closed)
+            {
+                yhteys.Open();
+            }
+        }
+        // Luodaan funktio yhteyden sulkemista varten
+        public void suljeYhteys()
+        {
+            if (yhteys.State == ConnectionState.Open)
+            {
+                yhteys.Close();
+            }
+        }
+    }
+}
