90839d705f51d5858fcab68b9dedb56bfaefdff2
katrimari <katri.lepola@edu.keuda.fi>
2022-05-05 13:08:10 +0300

    Back up

---
+++ StudyPoint_2.0_BackUp/Studypoint_2.0/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Studypoint_2._0
+{
+    public partial class StudypointFM : Form
+    {
+        KIRJAUDU kirjaudu = new KIRJAUDU();
+        public StudypointFM()
+        {
+            InitializeComponent();
+        }
+
+        private void KirjauduBT_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void RekisteroidyBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OtayhteyttaBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UuttaBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MeistaBT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kirjaudu1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Etusivu01BT_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void etusivu1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ StudyPoint_2.0_BackUp/Studypoint_2.0/PALVELUMME.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Studypoint_2._0
+{
+    public partial class PALVELUMME : UserControl
+    {
+        public PALVELUMME()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
